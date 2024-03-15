2c665e33e6a420701210de763e557c8658354fe7
patryk-pi <patryk.piasecki1@gmail.com>
2024-03-15 17:18:16 +0100

    created StyledCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Container } from "./components/styles/Container.styled";
 import Header from "./components/Header";
+import Card from "./components/Card";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./components/styles/Global";
+import content from "./content";
 
 const theme = {
     colors: {
@@ -19,7 +21,9 @@ function App() {
             <GlobalStyles />
             <Header />
             <Container>
-                <h1>Hello World</h1>
+                {content.map((item, index) => (
+                    <Card key={index} item={item} />
+                ))}
             </Container>
         </ThemeProvider>
     );
